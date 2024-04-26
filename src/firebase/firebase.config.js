560370d5f3d00f9a6a10a8be19e9b72c127f40c7
rahidhaque <ontirahid@yahoo.com>
2024-04-27 01:03:00 +0600

    Env variable created to hide firebase config

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,18 +4,22 @@ import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+
 // Your web app's Firebase configuration
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAdHAlvEZUoDzsTt2Jrh2unqsxUyW_Six8",
-  authDomain: "travel-tribe-1cef3.firebaseapp.com",
-  projectId: "travel-tribe-1cef3",
-  storageBucket: "travel-tribe-1cef3.appspot.com",
-  messagingSenderId: "768651134364",
-  appId: "1:768651134364:web:035cc9d91899a96fa93459"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 const auth = getAuth(app);
 
 export default auth;
