c2f48a65c773e793999df751c73c3ad0964204a7
rahidhaque <ontirahid@yahoo.com>
2024-04-29 04:52:59 +0600

    Sorting functionality added in all tourist spot

--- src/pages/Spots/Spots.jsx
+++ src/pages/Spots/Spots.jsx
@@ -1,13 +1,31 @@
 import { useLoaderData } from "react-router-dom";
 import Spot from "../Spot/Spot";
+import { useState } from "react";
 
 const Spots = () => {
   const spots = useLoaderData();
+
+  const [sortByAverageCost, setSortByAverageCost] = useState(false);
+
+  const handleSortByAverageCost = () => {
+    setSortByAverageCost(!sortByAverageCost);
+  };
+
+  let sortedSpots = [...spots];
+
+  if (sortByAverageCost) {
+    sortedSpots.sort((a, b) => a.averageCost - b.averageCost);
+  }
+
   return (
     <div>
-      <h1>Total Spots: {spots.length}</h1>
+      <div className="flex justify-center mb-5">
+        <button className="btn btn-accent" onClick={handleSortByAverageCost}>
+          {sortByAverageCost ? "Show Original Data" : "Sort By Average Cost"}
+        </button>
+      </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {spots.map((spot) => (
+        {sortedSpots.map((spot) => (
           <Spot key={spot._id} spot={spot}></Spot>
         ))}
       </div>
