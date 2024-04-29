86831465863a46609cd108ab2f15bfa8a19f6019
rahidhaque <ontirahid@yahoo.com>
2024-04-30 04:31:32 +0600

    Home Load data while fetching implemented

--- src/pages/Home/Countries.jsx
+++ src/pages/Home/Countries.jsx
@@ -3,23 +3,31 @@ import Country from "./Country";
 
 const Countries = () => {
   const [countries, setCountries] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     fetch("https://tourism-management-server-kappa.vercel.app/countries")
       .then((res) => res.json())
       .then((data) => {
         setCountries(data);
+        setIsLoading(false);
       });
   }, []);
 
   return (
     <div>
       <h2 className="text-center font-bold text-4xl my-10">Countries</h2>
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 px-4 space-y-4 md:space-y-0">
-        {countries.map((country) => (
-          <Country key={country._id} country={country}></Country>
-        ))}
-      </div>
+      {isLoading ? (
+        <p className="text-center mt-5">
+          <span className="loading loading-ring loading-lg font-bold"></span>
+        </p>
+      ) : (
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 px-4 space-y-4 md:space-y-0">
+          {countries.map((country) => (
+            <Country key={country._id} country={country}></Country>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
--- src/pages/Home/TouristSpots.jsx
+++ src/pages/Home/TouristSpots.jsx
@@ -3,11 +3,13 @@ import TouristSpot from "./TouristSpot";
 
 const TouristSpots = () => {
   const [spots, setSpots] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     fetch("https://tourism-management-server-kappa.vercel.app/spots")
       .then((res) => res.json())
       .then((data) => {
         setSpots(data);
+        setIsLoading(false);
       });
   }, []);
 
@@ -16,11 +18,17 @@ const TouristSpots = () => {
   return (
     <div>
       <h2 className="text-center font-bold text-4xl mt-10">Tourist Spots</h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {sixSpots.map((spot) => (
-          <TouristSpot key={spot._id} spot={spot}></TouristSpot>
-        ))}
-      </div>
+      {isLoading ? (
+        <p className="text-center mt-5">
+          <span className="loading loading-ring loading-lg font-bold"></span>
+        </p>
+      ) : (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {sixSpots.map((spot) => (
+            <TouristSpot key={spot._id} spot={spot}></TouristSpot>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
